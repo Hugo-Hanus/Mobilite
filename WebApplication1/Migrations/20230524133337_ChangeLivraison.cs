d70ae9a26c2afc1189e054ec6933cf6bf8387a88
Timzz <t.serb@student.helmo.be>
2023-05-24 16:23:26 +0200

    Mise en place validation livraison et affichage livraison pour chauffeur

---
+++ WebApplication1/Migrations/20230524133337_ChangeLivraison.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeLivraison : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DateDechargementEffective",
+                table: "Livraison",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "HeureDechargementEffective",
+                table: "Livraison",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateDechargementEffective",
+                table: "Livraison");
+
+            migrationBuilder.DropColumn(
+                name: "HeureDechargementEffective",
+                table: "Livraison");
+        }
+    }
+}
